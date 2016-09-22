5f2b7d135cdb29b386646861801304fd8dda84e0
Niclas Lund <Niclas Lund>
2016-09-22 18:03:54 +0200

    Assignment 2 done. Assignment 1 corrected.

--- code/prime.c
+++ code/prime.c
@@ -16,6 +16,7 @@ int is_prime(int n){
 	for(int i=2; i <= n/2; ++i){
 		//Om delbart så är det ej ett primtal
 		if(n % i == 0){
+
 			flag = 1;
 			break;
 		}
@@ -23,7 +24,8 @@ int is_prime(int n){
 			flag = 0;
 		}
 	}
-	if(flag == 0){
+	//Om flaggad, alltså ett primtal, returnera 1.
+	if(flag == 0 && n != 0 && n != 1){
 		return 1;
 	}
 	else{
@@ -35,4 +37,7 @@ int main(void){
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
   printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
   printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+  printf("%d\n", is_prime(1));
+  printf("%d\n", is_prime(0));
+  printf("%d\n", is_prime(2));
 }
--- code/print-primes.c
+++ code/print-primes.c
@@ -10,27 +10,62 @@
 #include <stdlib.h>
 
 #define COLUMNS 6
+int columncheck=0;
+int primeamount=0;
 
+void print_number(int n){
+  //i < n eller i == n?
+      printf("%10d ", n);
+    if(primeamount % COLUMNS ==0){
+      columncheck++;
+      printf("\n");
+  }
+}
 
+int is_prime(int n){
+  int flag;
+  for(int i=2; i <= n/2; ++i){
+    //Om delbart så är det ej ett primtal
+    if(n % i == 0){
+
+      flag = 1;
+      break;
+    }
+    else{
+      flag = 0;
+    }
+  }
+  //Om flaggad, alltså ett primtal, returnera 1.
+  if(flag == 0 && n != 0 && n != 1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 void print_primes(int n){
   // Should print out all prime numbers less than 'n'
   // with the following formatting. Note that
   // the number of columns is stated in the define
   // COLUMNS
+  for(int i = 0; i < n; i++){
+    if(is_prime(i)==1){
+      print_number(i);
+      primeamount++;
+    }
+  }
+//   printf("%10d ", 2);
+//   printf("%10d ", 3);
+//   printf("%10d ", 5);
+//   printf("%10d ", 7);
+//   printf("%10d ", 11);
+//   printf("%10d ", 13);
+//   printf("\n");
+//   printf("%10d ", 17);
+//   printf("%10d ", 19);
 
-  printf("%10d ", 2);
-  printf("%10d ", 3);
-  printf("%10d ", 5);
-  printf("%10d ", 7);
-  printf("%10d ", 11);
-  printf("%10d ", 13);
-  printf("\n");
-  printf("%10d ", 17);
-  printf("%10d ", 19);
-
-  printf("\n");
+//   printf("\n");
 }
-
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
