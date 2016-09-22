671a3d48112d000bbb2909f6b8f952c548b92aa1
Niclas Lund <Niclas Lund>
2016-09-22 15:44:34 +0200

    Assignment 1 done.

--- code/prime.c
+++ code/prime.c
@@ -9,7 +9,26 @@
 #include <stdio.h>
 
 int is_prime(int n){
-  return 0;
+	//Skriv om denna kod så att det returneras en etta ifall det är ett primtal
+	//och en nolla om det inte är det.
+	//Använd endast enkla loopar och if-satser
+	int flag;
+	for(int i=2; i <= n/2; ++i){
+		//Om delbart så är det ej ett primtal
+		if(n % i == 0){
+			flag = 1;
+			break;
+		}
+		else{
+			flag = 0;
+		}
+	}
+	if(flag == 0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 int main(void){
